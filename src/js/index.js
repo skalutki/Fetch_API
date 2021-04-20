84e2e4a3e221b036ddc4d1b1873e84e490cf7634
skalutki <skalutki@gmail.com>
2021-04-20 10:00:55 +0100

    fetch api works // first leyout for 320px ok

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,28 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+
+
+fetch('https://api.github.com/users/skalutki/repos?sort=created')
+.then( resp => resp.json())
+.then( resp => {
+    for (let repo of resp){
+        const {name, html_url, description, homepage } = repo;
+        const projects = document.querySelector('.projects--js');
+        const myTemplate = `
+        <section class="project">
+            <h2 class="project__title">${name}</h2>
+            <p class="project__description">${description}</p>
+                <footer class="project__footer">
+                    <a class="footer__link" href="${homepage}" title="Link do projektu ${name}">demo</a>
+                    <a class="footer__link" href="${html_url}" title="Link do repozytorium z projektu ${name}">repozytorium</a>
+                </footer>
+        </section>`;
+        projects.innerHTML += myTemplate;
+
+    }
+})
+.catch(error => {
+    console.log('Nie udało się pobrać danych z serwera');
+}) 
+
